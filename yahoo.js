5f13a68badbcaf789203a464835645bd24310835
KunatsKaryna <97902168+KunatsKaryna@users.noreply.github.com>
2022-02-01 21:16:15 +0300

    Add files via upload

--- yahoo.js
+++ yahoo.js
@@ -1,119 +1,83 @@
 describe('Checking mail progress', function() {
  
-
   it('should check that registered user can login to the mailbox',async function() {
-    await browser.url ("https://www.yahoo.com/");
-   
-    let signinButton = await $('//*[@id="ybar-inner-wrap"]/div[3]/div/div[3]/div[1]/div/a');
-    await signinButton.click();
-      
-    let loginButton = await $('//*[@id="login-username"]');
-    await loginButton.click();
-    let text = ('test12082011');
-    await loginButton.keys(text);
-
-    let nextButton = await $('//*[@id="login-signin"]');
-     await nextButton.click();
-
-     let passwordButton = await $('//*[@id="login-passwd"]');
-     await passwordButton.click();
-     let text2 = ('770021rl');
-    await passwordButton.keys(text2);
-
-    let nextButton2 = await $('//*[@id="login-signin"]');
-     await nextButton2.click();
-
-     let mailButton = await $('//*[@id="ybarMailLink"]/span[1]');
-     await mailButton.click();
-
+      await browser.url ("https://www.yahoo.com/");
+      await $('//*[@id="ybar-inner-wrap"]/div[3]/div/div[3]/div[1]/div/a').click();
+    let loginField = await $('//*[@id="login-username"]');
+      await loginField.click();
+    let login = ('test12082011');
+      await loginField.keys(login);
+      await $('//*[@id="login-signin"]').click();
+    let passwordField = await $('//*[@id="login-passwd"]');
+      await passwordField.click();
+    let password = ('770021rl');
+      await passwordField.keys(password);
+      await $('//*[@id="login-signin"]').click();
+      expect ('//*[@id="login-signin"]').toExist();
+      await $('//*[@id="ybarMailLink"]/span[1]').click();
+      expect ('//*[@id="ybarMailLink"]/span[1]').toExist();
     });
 
-     it('should check that registered user can create a new mail',async function() {
-
-      let writeButton = await $('//*[@id="app"]/div[2]/div/div[1]/nav/div/div[1]/a');
-     await writeButton.click();
-
-     let whomField = await $('//*[@id="message-to-field"]');
-     let text3 = ('test12082011@yahoo.com');
-    await whomField.keys(text3);
-
-
-    let themeField = await $('//*[@id="mail-app-component"]/div/div/div[1]/div[3]/div/div/input');
-    await themeField.click();
-    let text4 = ('letter');
-    await themeField.keys(text4);
-
+    it('should check that registered user can create a new mail',async function() {
+      await $('//*[@id="app"]/div[2]/div/div[1]/nav/div/div[1]/a').click();
+    let whomField = await $('//*[@id="message-to-field"]');
+    let whom = ('test12082011@yahoo.com');
+      await whomField.keys(whom);
+      expect(whomField).toHaveTextContaining('test12082011@yahoo.com');
+    let themeField = await $('//input[@data-test-id="compose-subject"]');
+      await themeField.click();
+    let subject = ('letter');
+      await themeField.keys(subject);
+      expect(themeField).toHaveTextContaining('letter');
     let bodyField = await $('//*[@id="editor-container"]/div[1]');
-    await bodyField.click();
-     let text5 = ('Hello, tester');
-    await bodyField.keys(text5);
-    browser.pause(2000);
-
+      await bodyField.click();
+    let body = ('Hello, tester');
+      await bodyField.keys(body);
+      expect(bodyField).toHaveTextContaining('Hello, tester');
   });
 
   it('should check that the mail can be saved as a draft',async function() { 
-    let draftButton = await $('//*[@id="app"]/div[2]/div/div[1]/nav/div/div[3]/div[1]/ul/li[4]/div/a/span[1]/span/span/span');
-    await draftButton.click();
-    browser.pause(3000);
+      await $('//span[@data-test-folder-name="Draft"]').click();
+      await $('//span[@data-test-folder-name="Draft"]').waitForDisplayed({ timeout: 5000 });
   });
 
   it('should check that the mail presents as a draft',async function() { 
-  let draftLetter = await $('//*[@id="mail-app-component"]/div/div/div[2]/div/div/div[2]/div/div[1]/ul/li[2]/a/div/div[2]/div[1]');
-  await expect(draftLetter).toExist();
-  await draftLetter.click();
-
+    let draftLetter = await $('//span[@title="test12082011@yahoo.com"][1]');
+      expect(draftLetter).toExist();
+      await draftLetter.click();
 });
 
-it('should check that the draft contents adress,theme,body',async function() { 
-
-  let draftWhom = await $('//*[@id="mail-app-component"]/div/div/div[1]/div[2]/div/div');
-  await expect(draftWhom).toHaveTextContaining('test12082011@yahoo.com');
-
-  let draftTheme = await $('//input[@data-test-id="compose-subject"]');
-  let draftThemeAttr = await draftTheme.getAttribute('value');
-  expect(draftThemeAttr).toEqual('letter');
-
-  let draftbody = await $('//*[@id="editor-container"]/div[1]');
-  await expect(draftbody).toHaveTextContaining('Hello, tester');
-
+  it('should check that the draft contents adress,theme,body',async function() { 
+    let draftWhom = await $('//div[data-test-id="pill-text"]');
+      expect(draftWhom).toHaveTextContaining('test12082011@yahoo.com');
+    let draftTheme = await $('//input[@data-test-id="compose-subject"]');
+    let draftThemeAttr = await draftTheme.getAttribute('value');
+      expect(draftThemeAttr).toEqual('letter');
+    let draftbody = await $('//*[@id="editor-container"]/div[1]');
+      expect(draftbody).toHaveTextContaining('Hello, tester');
 });
 
-  it('should check that the draft was sent',async function() {
-
-  let sendButton = await $('//*[@id="mail-app-component"]/div/div/div[2]/div[2]/div/button');
-  await sendButton.click();
-
+  it('should check that the draft can be send',async function() {
+      await $('//button[@data-test-id="compose-send-button"]').click();
 }); 
 
-
   it('should check that the mail disappeared from drafts folder',async function() {
-    let draftButton = await $('//*[@id="app"]/div[2]/div/div[1]/nav/div/div[3]/div[1]/ul/li[4]/div/a'); 
-    await draftButton.click();
-
-    let draftMail = await $('//*[@id="mail-app-component"]/div/div/div[2]/div/div/div[2]/div/div[1]/ul/li[2]/a/div/div[2]/div[1]'); 
+      await $('//span[@title="test12082011@yahoo.com"][1]').click(); 
+    let draftMail = await $('//span[@title="test12082011@yahoo.com"][1]'); 
     let isDisplayed = await draftMail.isDisplayed(); 
-    expect(isDisplayed).toHaveValue(false);
-
+      expect(isDisplayed).toHaveValue(false);
 });
 
   it('should check that the mail is in sent folder',async function() {
-  let sentButton = await $('//*[@id="app"]/div[2]/div/div[1]/nav/div/div[3]/div[1]/ul/li[5]/div/a/span[1]/span/span/span'); 
-  await sentButton.click();
-  
-  let letter = await $('//*[@id="mail-app-component"]/div/div/div[2]/div/div/div[2]/div/div[1]/ul/li[2]/a/div');
-  await expect(letter).toExist();
-
+      await $('//a[@data-test-folder-name="Sent"]').click(); 
+    let letter = await $('//span[@data-test-id="message-subject"]');
+      expect(letter).toExist();
 });
 
-it('should check that the user can log off',async function() {
-  let profileButton = await $('#ybarAccountMenuOpener'); 
-  await profileButton.moveTo();
-
-  let logoffButton = await $('//*[@id="profile-signout-link"]');
-  await logoffButton.waitForDisplayed(); 
-  await logoffButton.click();
-
-
+  it('should check that the user can log off',async function() {
+      await $('#ybarAccountMenuOpener').moveTo();
+      await $('//*[@id="profile-signout-link"]').waitForDisplayed();
+      await $('//*[@id="profile-signout-link"]').click();
+      expect ('//*[@id="ybar-inner-wrap"]/div[3]/div/div[3]/div[1]/div/a').toExist();
 });
-
 });
